3ca31f0b01376a2488f1535458c56a535429021d
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-07 13:43:08 -0300

    Add project files.

---
+++ FizzBuzzApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                decimal r3 = i % 3;
+                decimal r5 = i % 5;
+
+                if (r3 == 0 && r5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (r3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (r5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
